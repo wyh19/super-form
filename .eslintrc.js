ee1885209c8196404088044776b2344d8b8534db
wyh_19 <wyh_19@163.com>
2021-05-22 22:16:43 +0800

    调整eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,15 +14,18 @@ module.exports = {
   // add your custom rules here
   //it is base on https://github.com/vuejs/eslint-config-vue
   rules: {
-    "vue/max-attributes-per-line": [2, {
+    "vue/max-attributes-per-line": [0, {
       "singleline": 10,
       "multiline": {
         "max": 1,
         "allowFirstLine": false
       }
     }],
+    "vue/html-closing-bracket-newline":"off",
+    "vue/html-self-closing": "off",
+    "vue/require-default-prop": "off",
     "vue/singleline-html-element-content-newline": "off",
-    "vue/multiline-html-element-content-newline":"off",
+    "vue/multiline-html-element-content-newline": "off",
     "vue/name-property-casing": ["error", "PascalCase"],
     "vue/no-v-html": "off",
     'accessor-pairs': 2,
@@ -47,7 +50,7 @@ module.exports = {
     'curly': [2, 'multi-line'],
     'dot-location': [2, 'property'],
     'eol-last': 2,
-    'eqeqeq': ["error", "always", {"null": "ignore"}],
+    'eqeqeq': ["error", "always", { "null": "ignore" }],
     'generator-star-spacing': [2, {
       'before': true,
       'after': true
@@ -189,7 +192,7 @@ module.exports = {
     'yield-star-spacing': [2, 'both'],
     'yoda': [2, 'never'],
     'prefer-const': 2,
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-debugger': 1,
     'object-curly-spacing': [2, 'always', {
       objectsInObjects: false
     }],
