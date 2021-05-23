0c07677f36b4ffcc459aebb4e4d1e82f9088e2c3
wyh_19 <wyh_19@163.com>
2021-05-23 10:19:14 +0800

    实现节点解析

--- src/components/anchor/index.vue
+++ src/components/anchor/index.vue
@@ -5,7 +5,7 @@
 -->
 <template>
   <div class="anchor">
-    <div v-for="node in anchorNodes" :key="node.label" :label="node.index"
+    <div v-for="node in sections" :key="node.label" :label="node.index"
          :class="[node.ismain?'anchor-main-node':'anchor-sub-node']">
       {{ node.label }}
     </div>
@@ -14,19 +14,53 @@
 
 <script>
 export default {
+  props: {
+    pageBlock: HTMLElement
+  },
   data() {
     return {
-      anchorNodes: [
-        { label: '基础信息', ismain: true, index: '1' },
-        { label: '个人信息', index: '1.1' },
-        { label: '其他信息', index: '1.2' },
-        { label: '高级信息', ismain: true, index: '2' },
-        { label: 'xxx信息', index: '2.1' }
-      ]
+      sections: []
     }
   },
+  mounted() {
+    this.sections = this.getSectionsData(this.pageBlock)
+    this.pageBlock.addEventListener('scroll', this.handlePageScroll)
+  },
+  beforeDestroy() {
+    this.pageBlock.removeEventListener('scroll', this.handlePageScroll)
+  },
   methods: {
-    handleClick(label) {
+    // 从pageBlock中获取章节信息
+    getSectionsData(pageBlock) {
+      let mainIndex = 0 // 主节点的数字序号
+      let subIndex = 0 // 子节点的数字序号
+      // 查询出data-section的节点，并转化成数组
+      const sections = Array.from(pageBlock.querySelectorAll('[data-section]'))
+      // map转化节点数组为最终的数据
+      return sections.map((item, index) => {
+        let ismain = false
+        if ('ismain' in item.dataset) {
+          ismain = true
+          mainIndex++
+          // 遇到新的主节点，重置subIndex
+          subIndex = 0
+        } else {
+          subIndex++
+        }
+        return {
+          ismain,
+          index: ismain ? mainIndex : `${mainIndex}.${subIndex}`,
+          label: item.dataset.section
+        }
+      })
+    },
+    // 页面的滚动事件处理函数
+    handlePageScroll(e) {
+      e.stopPropagation()
+      this.currentSection = this.getCurrentSection()
+    },
+    // 计算出当前滚动到的章节
+    getCurrentSection() {
 
     }
   }
--- src/views/super-form/components/the-form/index.vue
+++ src/views/super-form/components/the-form/index.vue
@@ -4,12 +4,18 @@
  * @Date: 2021-05-21
 -->
 <template>
-  <div class="form-wrapper">
+  <div ref="pageBlock" class="form-wrapper">
     <el-button type="primary" @click="handleSave">保存</el-button>
+    <div data-section="基础信息" data-ismain></div>
+    <div data-section="个人信息"></div>
     <form1 ref="form1" :data="formDataMap.form1" />
+    <div data-section="其他信息"></div>
+    <div style="width:300px;height:200px;background:#ccc;">占位符</div>
+    <div data-section="高级信息" data-ismain></div>
+    <div data-section="公司信息"></div>
     <form2 ref="form2" :data="formDataMap.form2" />
     <div class="anchor-wrapper">
-      <anchor />
+      <anchor v-if="pageBlock" :page-block="pageBlock" />
     </div>
   </div>
 </template>
@@ -27,6 +33,7 @@ export default {
   },
   data() {
     return {
+      pageBlock: null,
       formDataMap: {
         form1: {
           name: 'wyh',
@@ -35,6 +42,9 @@ export default {
         form2: {}
       }
     }
+  },
+  mounted() {
+    this.pageBlock = this.$refs['pageBlock']
   },
   methods: {
     handleSave() {
@@ -77,5 +87,31 @@ export default {
   height: 300px;
   top: 50%;
   transform: translate(0, -50%);
+}
+div[data-section] {
+  position: relative;
+  font-size: 14px;
+  font-weight: bold;
+  color: #5c658d;
+  padding: 14px 0;
+  margin-left: 34px;
+  &::before {
+    content: attr(data-section);
+  }
+}
+div[data-ismain] {
+  font-size: 16px;
+  font-weight: bold;
+  margin-left: 28px;
+  &::after {
+    content: '';
+    position: absolute;
+    left: -16px;
+    top: 14px;
+    width: 4px;
+    height: 16px;
+    background: #5c658d;
+    border-radius: 2px;
+  }
 }
 </style>
